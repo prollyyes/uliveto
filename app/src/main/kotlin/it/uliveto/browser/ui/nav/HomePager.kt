e970e64a29bea9b479432f622dc7ab6c5b8cc51a
Eddie <127027878+prollyyes@users.noreply.github.com>
2026-06-08 02:01:05 +0200

    polish: HomePager spring snap + page parallax

--- app/src/main/kotlin/it/uliveto/browser/ui/nav/HomePager.kt
+++ app/src/main/kotlin/it/uliveto/browser/ui/nav/HomePager.kt
@@ -1,12 +1,17 @@
 package it.uliveto.browser.ui.nav
 
 import androidx.activity.compose.BackHandler
+import androidx.compose.animation.core.spring
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.pager.HorizontalPager
+import androidx.compose.foundation.pager.PagerDefaults
 import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.unit.dp
 import it.uliveto.browser.tabs.TabManager
 import it.uliveto.browser.ui.screens.bookmarks.BookmarksScreen
 import it.uliveto.browser.ui.screens.bookmarks.BookmarksViewModel
@@ -27,6 +32,11 @@ fun HomePager(
     val pagerState = rememberPagerState(initialPage = 1, pageCount = { 3 })
     val scope = rememberCoroutineScope()
 
+    val snapFlingBehavior = PagerDefaults.flingBehavior(
+        state = pagerState,
+        snapAnimationSpec = spring(dampingRatio = 0.78f, stiffness = 350f),
+    )
+
     // Hardware back on a side page scrolls home instead of exiting the app
     BackHandler(enabled = pagerState.currentPage != 1) {
         scope.launch { pagerState.animateScrollToPage(1) }
@@ -34,30 +44,38 @@ fun HomePager(
 
     HorizontalPager(
         state = pagerState,
+        flingBehavior = snapFlingBehavior,
         modifier = Modifier.fillMaxSize(),
     ) { page ->
-        when (page) {
-            0 -> TabsScreen(
-                onSelectTab = onSelectTab,
-                onCloseTab = { tabId ->
-                    TabManager.closeTab(tabId)
-                    if (TabManager.tabs.isEmpty()) scope.launch { pagerState.animateScrollToPage(1) }
-                },
-                onNewTab = { scope.launch { pagerState.animateScrollToPage(1) } },
-                onBack = { scope.launch { pagerState.animateScrollToPage(1) } },
-            )
-            1 -> StartScreen(
-                viewModel = startViewModel,
-                onNavigateToBrowser = onNavigateToBrowser,
-                onNavigateToTabs = { scope.launch { pagerState.animateScrollToPage(0) } },
-                onNavigateToBookmarks = { scope.launch { pagerState.animateScrollToPage(2) } },
-                onNavigateToSettings = onNavigateToSettings,
-            )
-            2 -> BookmarksScreen(
-                viewModel = bookmarksViewModel,
-                onOpenUrl = onNavigateToBrowser,
-                onBack = { scope.launch { pagerState.animateScrollToPage(1) } },
-            )
+        val pageOffset = (pagerState.currentPage - page) + pagerState.currentPageOffsetFraction
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .graphicsLayer { translationX = pageOffset * 40.dp.toPx() },
+        ) {
+            when (page) {
+                0 -> TabsScreen(
+                    onSelectTab = onSelectTab,
+                    onCloseTab = { tabId ->
+                        TabManager.closeTab(tabId)
+                        if (TabManager.tabs.isEmpty()) scope.launch { pagerState.animateScrollToPage(1) }
+                    },
+                    onNewTab = { scope.launch { pagerState.animateScrollToPage(1) } },
+                    onBack = { scope.launch { pagerState.animateScrollToPage(1) } },
+                )
+                1 -> StartScreen(
+                    viewModel = startViewModel,
+                    onNavigateToBrowser = onNavigateToBrowser,
+                    onNavigateToTabs = { scope.launch { pagerState.animateScrollToPage(0) } },
+                    onNavigateToBookmarks = { scope.launch { pagerState.animateScrollToPage(2) } },
+                    onNavigateToSettings = onNavigateToSettings,
+                )
+                2 -> BookmarksScreen(
+                    viewModel = bookmarksViewModel,
+                    onOpenUrl = onNavigateToBrowser,
+                    onBack = { scope.launch { pagerState.animateScrollToPage(1) } },
+                )
+            }
         }
     }
 }
